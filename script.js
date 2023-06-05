764a722d275b685475f327a3297886bf019127f1
Rad <118402014+DarrGilreath@users.noreply.github.com>
2023-06-05 02:01:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const loader = document.querySelector('.loader');
 
 // NASA API
 const count = 10;
-const apiKey = 'f72wxXQjKPsSOzhR5oMGrlKWsl3zpg9o5tS13J68';
+const apiKey = 'DEMO_KEY';
 const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
 
 let resultsArray = [];
