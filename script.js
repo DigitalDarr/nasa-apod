373f260528b3bcf3d5639f56bb4532598f7a3dbc
Darr <118402014+DigitalDarr@users.noreply.github.com>
2024-12-08 21:05:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -121,19 +121,6 @@ function saveFavorite(itemUrl) {
   });
 }
 
-// DarkMode toggle
-function toggleDarkMode() {
-  let isDark = document.body.classList.toggle('dark-mode');
-  localStorage.setItem('darkMode', isDark ? 'enabled' : 'disabled');
-}
-
-// On page load
-document.addEventListener('DOMContentLoaded', (event) => {
-  if (localStorage.getItem('darkMode') === 'enabled') {
-    document.body.classList.add('dark-mode');
-  }
-});
-
 // Remove item from Favorites
 function removeFavorite(itemUrl) {
   if (favorites[itemUrl]) {
